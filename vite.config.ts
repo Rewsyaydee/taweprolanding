4df31502036185f9336cf1560b1cf1d3caa315b8
PuteraSedi <133309207+Rewsyaydee@users.noreply.github.com>
2026-07-29 01:56:12 +0800

    fix: remove untyped Node process dependency

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? "/taweprolanding/" : "/",
+  base: "/",
   build: {
     target: "es2020",
     cssCodeSplit: true,
